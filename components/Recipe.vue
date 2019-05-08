ec1fcc3d5089b188d7e03c36ffe34d90d9938c89
mirv <reski.raptor@gmail.com>
2019-05-08 01:49:53 -0500

    status save of moving about to index before implementing component in it 28:33

---
+++ components/Recipe.vue
@@ -0,0 +1,13 @@
+<template>
+  <artitle class="recipe">
+      <div class="thumbnail" :style="{backgroundImage: 'url(' + thumbnail + ')}'></div>
+      <h1> {{ title }}</h1>
+      <p> {{ previewText }} </p>
+  </artitle>
+</template>
+
+<script>
+export default {
+  props: ['thumbnail', 'title', 'previewText']
+}
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,37 @@
+<template>
+    <section class="recipes">
+        <artitle class="recipe">
+            <div class="thumbnail" style="background-image: url('http://stmedia.stimg.co/OC-FairStick_wide.00_00_37_18.Still016.jpg?h=630&w=1200&fit=crop&bg=999&crop=faces')"></div>
+                <h1>Title</h1>
+                <p>Some nice preview text</p>
+        </artitle>
+    </section>
+
+  
+</template>
+
+<style scoped>
+    .recipes {
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+
+<style scoped>
+.recipe {
+    box-sizing: border-box;
+    width: 280px;
+    padding: 8px;
+    border: 1px solid #ccc;
+    box-shadow: 0 2px 2px #eee;
+}
+
+.thumbnail {
+    background-position: center;
+    background-size: cover;
+    width: 100%;
+    height: 200px;
+}
+</style>
